3864ecc2520ab5c63a976780efd70a8fe7296ecc
Franek Zawada <franek.zawada@gmail.com>
2020-02-23 19:27:36 +0100

    Add elif for computer

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
-var computerMove
-computerMove = 'kamień'
+var computerMove, playerMove, randomNumber;
 
-var playerMove
-playerMove = 'papier'
+playerMove = '____'
+
+randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+if (randomNumber == '1') {
+  computerMove = 'kamień';
+} else if (randomNumber == '2') {
+  computerMove = 'papier';
+} else if (randomNumber == '3') {
+  computerMove = 'nożyce';
+}
 
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
