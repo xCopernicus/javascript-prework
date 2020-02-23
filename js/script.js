046fd7a0ba764bf48dfc2114412eddd851575042
Franek Zawada <franek.zawada@gmail.com>
2020-02-23 16:01:06 +0100

    Add 2 variables

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var computerMove
+computerMove = 'kamień'
+
+var playerMove
+playerMove = 'papier'
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
