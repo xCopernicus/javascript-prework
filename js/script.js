ad0c6a2076fc3ddd8bc9d08a322ef4f87758832b
Franek Zawada <franek.zawada@gmail.com>
2020-02-25 15:06:32 +0100

    Add buttons Add styles Fixed js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,11 @@
-var player1Name, player2Name player1, player2, moveId, computerMove, playerMove, randomNumber, playerInput;
+var buttonScissors, buttonPaper, buttonRock, buttonName, player1Name, player2Name, player1, player2, moveId, computerMove, playerMove, randomNumber, playerInput;
+
+buttonRock = document.getElementById('button-rock');
+buttonPaper = document.getElementById('button-paper');
+buttonScissors = document.getElementById('button-scissors');
+
+player1Name = 'Computer'
+player2Name = 'Player'
 
 function getMoveName(moveId) {
   console.log('function getMoveName called with argument: ' + moveId);
@@ -8,24 +15,21 @@ function getMoveName(moveId) {
     return 'paper';
   } else if (moveId == '3') {
     return 'scissors';
-  } else {
-    printMessage('Wrong number inserted was changed to 1: rock')
-    return 'rock';
   }
 }
 
-function displayResult(player1, player2, player1Name, player2Name) {
+function displayResult(player1, player2) {
   console.log('function displayResult called with argument: ' + player1 + ', ' + player2);
-  printMessage(player1Name + ' selected ' + player1 + ', and ' + player2Name + ' selected ' + player2);
+  printMessage(player1Name + ': ' + player1 + ', and ' + player2Name + ': ' + player2);
   if (player1 == player2) {
     printMessage('It\'s a tie!');
-  } else if (player1 == 'rock' && player2 == 'paper') {
+  } else if (player1 == 'rock' && player2 == 'scissors') {
     printMessage(player1Name + ' wins!');
   } else if (player1 == 'paper' && player2 == 'rock') {
     printMessage(player1Name + ' wins!');
   } else if (player1 == 'scissors' && player2 == 'paper') {
     printMessage(player1Name + ' wins!');
-  } else if (player2 == 'rock' && player1 == 'paper') {
+  } else if (player2 == 'rock' && player1 == 'scissors') {
     printMessage(player2Name + ' wins!');
   } else if (player2 == 'paper' && player1 == 'rock') {
     printMessage(player2Name + ' wins!');
@@ -34,14 +38,16 @@ function displayResult(player1, player2, player1Name, player2Name) {
   }
 }
 
-randomNumber = Math.floor(Math.random() * 3 + 1);
-console.log('Computer selected: ' + randomNumber);
+function buttonClicked(buttonName) {
+  clearMessages();
+  randomNumber = Math.floor(Math.random() * 3 + 1);
+  console.log(player1Name + ' selected: ' + randomNumber);
+  console.log(player2Name + ' selected: ' + buttonName);
+  computerMove = getMoveName(randomNumber);
+  playerMove = buttonName;
+  displayResult(computerMove, playerMove);
+}
 
-playerInput = prompt('Your turn, select! 1: rock, 2: paper, 3: scissors.');
-console.log('Player selected: ' + playerInput);
-
-computerMove = getMoveName(randomNumber)
-
-playerMove = getMoveName(playerInput)
-
-displayResult(computerMove, playerMove, 'Computer', 'Player')
+buttonRock.addEventListener('click', function(){ buttonClicked('rock'); });
+buttonPaper.addEventListener('click', function(){ buttonClicked('paper'); });
+buttonScissors.addEventListener('click', function(){ buttonClicked('scissors'); });
